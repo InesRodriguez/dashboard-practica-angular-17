e420483cabde4c7bbb3326f07661fcb07a3e0536
Ines Alexandra Rodriguez <alexandra.rodriguez@soytul.com>
2023-12-27 15:47:19 -0300

    feat: initial commit

---
+++ src/app/shared/heavy-loarders/heavy-loarders-fast.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heavy-loarders-fast',
+  standalone: true,
+  imports: [
+    CommonModule,
+  ],
+  template: ``,
+  styles: ``,
+})
+export class HeavyLoardersFastComponent { }
---
+++ src/app/shared/heavy-loarders/users-loarder.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-loader',
+  standalone: true,
+  imports: [
+    CommonModule,
+  ],
+  template: `<h1>hola mundo!</h1>`,
+  styles: ``,
+})
+export class UsersLoarderComponent { }
