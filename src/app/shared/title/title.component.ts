a1d9e054d99bc44d1cc36a4abf64f0e43c9f5a13
Ines Alexandra Rodriguez <alexandra.rodriguez@soytul.com>
2023-12-28 19:54:04 -0300

    feat: componente de titulo y configuracion atributo

--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -1,17 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input, booleanAttribute } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
-  templateUrl: './title.component.html',
-  styles: `
-    :host {
-      display: block;
-    }
-  `,
+  imports: [CommonModule],
+  template: `<h1 class="text-3xl mb-5">{{title}} - {{withShadow}}</h1>`,
+  styles: ``
 })
-export class TitleComponent { }
+export class TitleComponent {
+  @Input({ required: true }) title!: string;
+  @Input({transform:booleanAttribute}) withShadow: boolean = false;
+}
