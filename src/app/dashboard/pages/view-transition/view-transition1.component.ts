648a4645b254f4f173d2992f4518130f80fc72e3
Ines Alexandra Rodriguez <alexandra.rodriguez@soytul.com>
2024-01-03 14:25:19 -0300

    feat: view transition + hero animation

---
+++ src/app/dashboard/pages/view-transition/view-transition1.component.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
+
+@Component({
+  selector: 'app-view-transition',
+  standalone: true,
+  imports: [CommonModule, TitleComponent],
+  template: `
+    <app-title title="View transition 1" />
+    <section class="flex justify-start">
+      <img srcset="https://picsum.photos/id/237/200/300" 
+      alt="Picture"
+      with="200"
+      height="300"
+      style="view-transition-name: hero1"
+     
+      />
+      <div class="bg-blue-500 w-56 h-58"  style="view-transition-name: hero2"></div>
+    </section>
+  `
+})
+export default class ViewTransition1Component {}
